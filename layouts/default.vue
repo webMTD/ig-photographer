17eb6f40bc71e96aaec01bdf1b21226620776881
Daria Kriukova <kda.dariush@gmail.com>
2018-04-15 22:49:48 +0300

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
+    <nav class="navbar is-fixed-bottom is-light">
+      <div class="container">
+      <div class="navbar-menu" id="nav-menu">
+          <div class="navbar-start">
+          </div>
+          <nuxt-link to="/portfolio" class="navbar-item">
+            Портфолио
+          </nuxt-link>
+          <nuxt-link to="/blog" class="navbar-item">
+            Блог
+          </nuxt-link>
+          <nuxt-link to="/faq" class="navbar-item">
+            ЧаВо
+          </nuxt-link>
+          <nuxt-link to="/contacts" class="navbar-item">
+            Контакты
+          </nuxt-link>
+          <div class="navbar-end">
+          </div>
+        </div>
+        <div class="navbar-brand is-white">
+          <a role="button" class="navbar-burger burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true">
+            </span>
+            <span aria-hidden="true">
+            </span>
+            <span aria-hidden="true">
+            </span>
+          </a>
+        </div>
+      </div>
+    </nav>
     <nuxt/>
   </div>
 </template>
+
 
 <style>
 html
@@ -20,34 +53,5 @@ html
 {
   box-sizing: border-box;
   margin: 0;
-}
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
