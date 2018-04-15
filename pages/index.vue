0913d7b0e9b4067273d771b3b40c9abeb930d5ab
Daria Kriukova <kda.dariush@gmail.com>
2018-04-15 22:48:27 +0300

    upd pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,90 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        photoi
-      </h1>
-      <h2 class="subtitle">
-        photographer website
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <!--<div class="container">
+    <div class="photo-category family-shot"></div>
+    <div class="photo-category wedding-shot"></div>
+    <div class="photo-category studio-shot"></div>
+    <div class="photo-category kid-shot"></div>
+  </div>-->
+  <div>
+    <div></div>
+  <div class="columns is-desktop">
+    <div class="column is-one-half-desktop"></div>
+    <div class="column is-desktop"></div>
+  </div>
+  <div class="columns is-desktop">
+    <div class="column is-one-half-desktop"></div>
+    <div class="column is-desktop"></div>
+  </div>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
+export default{
+  layout: 'default'
 }
 </script>
 
 <style>
-.container
-{
+.columns {
+  height: 50vh;
+}
+.column {
+  height: 100%;
+  border: 3px dashed red;
+}
+/*
+.container {
   min-height: 100vh;
+  max-height: 100vh;
+  min-width: 100vw;
+  max-width: 100vw;
   display: flex;
   justify-content: center;
-  align-items: center;
   text-align: center;
+  vertical-align: center;
+  flex-wrap: no-wrap;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+
+.photo-category {
+  min-width: 30%;
+  max-width: 30%;
+  min-height: 100vh;
+  transform: skew(-10deg);
+}
+
+.photo-category:hover {
+  filter: sepia(0.1);
+}
+
+.family-shot {
+  background-color:aqua;
+}
+
+.wedding-shot {
+  background-color: pink;
+  min-width: 27%;
+  max-width: 27%;
+}
+
+.studio-shot {
+  background-color: darkslateblue;
+  min-width: 27%;
+  max-width: 27%;
+}
+
+.kid-shot {
+  background-color: crimson;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
+*/
+
 </style>
+
