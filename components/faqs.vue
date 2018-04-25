74969c4f64af14698fb6f613cf75b1d9ecc09262
Tieriko <timnazarcev@gmail.com>
2018-04-25 21:29:47 +0300

    first version of template for aask/answer item

---
+++ components/faqs.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="container is-centered">
+    <div class="columns is-centered is-mobile">
+        <div v-on:click="isShow=!isShow" class="column is-11-touch is-half-desktop ask-container">
+            <div>{{ item.ask }}</div>
+        </div>
+    </div>
+    <div class="columns is-centered is-mobile">
+        <div v-show="isShow" class="column is-11-touch is-half-desktop answer-container">
+            <div>{{ item.answer }}</div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        props: [
+            item.ask,
+            item.answer
+        ],
+        data() {
+            return {   
+                isShow: false,
+            }
+        }
+    }
+</script>
