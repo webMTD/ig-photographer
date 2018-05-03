252aa6dc6bf60e2305b9012824b89bb5eb64bffc
Tieriko <timnazarcev@gmail.com>
2018-05-03 21:28:06 +0300

    Made template for Ask-Answer accordion in FAQ (very light)

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,58 +1,25 @@
 <template>
     <section class="section">
-        <div class="container is-centered faq-container">
-            <faqs></faqs>
-            <!--<div class="columns is-centered is-mobile">
-                <div v-on:click="isShow=!isShow" class="column is-11-touch is-half-desktop ask-container">
-                    <div>Как забронировать Вас на свою дату?</div>
+        <div class="container">
+            <ul v-if="items.length">
+                <div
+                    v-for="item in items"
+                    v-bind:key="item.id"
+                >   
+                    <div class="columns is-centered is-mobile">
+                        <div v-on:click="item.isShow=!item.isShow" class="column is-11-touch is-half-desktop ask-container">
+                            <div>{{ item.questionProperty }}</div>
+                        </div>
+                    </div>
+                    <div class="columns is-centered is-mobile">
+                        <div v-show="item.isShow" class="column is-11-touch is-half-desktop answer-container">
+                            <div>{{ item.answerProperty }}</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="columns is-centered is-mobile">
-                <div v-show="isShow" class="column 0 is-11-touch is-half-desktop answer-container showAnswer">
-                    <div>Чтобы забронировать дату, Вам нужно внести аванс в размере 2000р от общей стоимости и подписать договор.</div>
-                </div>
-            </div>
-            <div class="columns is-centered is-mobile">
-                <div v-on:click="isShow=!isShow" class="column 1 is-11-touch is-half-desktop ask-container">
-                    <div>Можете ли вы порекомендовать визажистов, стилистов, флористов и оформителей?</div>
-                </div>
-            </div>
-            <div class="columns is-centered is-mobile">
-                <div v-show="isShow" class="column is-11-touch is-half-desktop answer-container showAnswer">
-                    <div>Я доверяю своих клиентов только профессионалам и обязательно посоветую вам нескольких специалистов в той или иной области.</div>
-                </div>
-            </div>
-            <div v-on:click="isShow=!isShow" class="columns is-centered is-mobile">
-                <div class="column is-11-touch is-half-desktop ask-container">
-                    <div>Печатаете ли вы фотокниги?</div>
-                </div>
-            </div>
-            <div v-show="isShow" class="columns is-centered is-mobile">
-                <div class="column is-11-touch is-half-desktop answer-container showAnswer">
-                    <div>Да. К каждому пакету можно дополнительно приобрести фотокнигу. Цена фотокниги зависит от ее размера, количества разворотов и материала обложки.</div>
-                </div>
-            </div> 
-            <div class="columns is-centered is-mobile">
-                <div class="column is-11-touch is-half-desktop ask-container">
-                    <div>Помогаете ли вы найти место для фотосъемки?</div>
-                </div>
-            </div>
-            <div class="columns is-centered is-mobile">
-                <div class="column is-11-touch is-half-desktop answer-container showAnswer">
-                    <div>Да, конечно. В наших общих интересах провести фотосъемку в красивом и оригинальном месте, которое подойдет именно Вам.</div>
-                </div>
-            </div>
-            <div class="columns is-centered is-mobile">
-                <div class="column is-11-touch is-half-desktop ask-container">
-                    <div>Сколько будут обрабатываться фотографии?</div>
-                </div>
-            </div>
-            <div class="columns is-centered is-mobile">
-                <div class="column is-11-touch is-half-desktop answer-container showAnswer">
-                    <div>Сроки обработки свадебных фотографий варьируются от 1,5 до 2 месяцев, в зависимости от занятости и месяца съемки.</div>
-                </div>
-            </div>-->
+            </ul>
         </div>
+        <nuxt/>
     </section>
 </template>
 
@@ -69,16 +36,45 @@
 </style>
 
 <script>
-import faqs from '~/components/faqs.vue'
+
+
 export default {
-    components: {
-        faqs
-    },
     data () {
-        item: [{
-            ask: 'Как забронировать Вас на свою дату?',
-            answer: 'Чтобы забронировать дату, Вам нужно внести аванс в размере 2000р от общей стоимости и подписать договор.'
-        }]
+        return {
+            items: [
+                {
+                    id: 0,
+                    questionProperty: "Как забронировать Вас на свою дату?",
+                    answerProperty: "Чтобы забронировать дату, Вам нужно внести аванс в размере 2000р от общей стоимости и подписать договор.",
+                    isShow: false
+                },
+                {
+                    id: 1,
+                    questionProperty: "Можете ли вы порекомендовать визажистов, стилистов, флористов и оформителей?",
+                    answerProperty: "Я доверяю своих клиентов только профессионалам и обязательно посоветую вам нескольких специалистов в той или иной области.",
+                    isShow: false
+                },
+                {
+                    id: 2,
+                    questionProperty: "Печатаете ли вы фотокниги?",
+                    answerProperty: "Да. К каждому пакету можно дополнительно приобрести фотокнигу. Цена фотокниги зависит от ее размера, количества разворотов и материала обложки.",
+                    isShow: false
+                },
+                {
+                    id: 3,
+                    questionProperty: "Помогаете ли вы найти место для фотосъемки?",
+                    answerProperty: "Да, конечно. В наших общих интересах провести фотосъемку в красивом и оригинальном месте, которое подойдет именно Вам.",
+                    isShow: false
+                },
+                {
+                    id: 4,
+                    questionProperty: "Сколько будут обрабатываться фотографии?",
+                    answerProperty: "Сроки обработки свадебных фотографий варьируются от 1,5 до 2 месяцев, в зависимости от занятости и месяца съемки.",
+                    isShow: false
+                },
+            ],
+            
+        }
     }
 }
 </script>
