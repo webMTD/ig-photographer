ff98c189d69bd72396c6a69aef80fcee1c0a1f78
Tieriko <timnazarcev@gmail.com>
2018-05-14 00:44:00 +0300

    first try of firestore storage

---
+++ pages/portfolio.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tabs is-centered is-boxed">
+      <ul>
+          <li class="is-active"><a>Портфолио</a></li>
+          <li><a>Семейная фотография</a></li>
+          <li><a>Детская фотография</a></li>
+          <li><a>Студийная фотография</a></li>
+          <li><a>Свадебная фотография</a></li>
+      </ul>
+  </div>
+
+</template>
+
+<script>
+import db from "~/firebase/init.js"
+import firebase from 'firebase'
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    // created(){
+    //     var storageRef = firebase.storage().ref("portfolio/wedding-shot/*.jpg");
+    //     storageRef.getDownloadURL().then((url) => {
+    //         console.log(url);
+    //     });
+    // }
+  
+}
+</script>
+
+<style>
+
+</style>
